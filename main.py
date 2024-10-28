c319e9805339d6e1378ba4e113ab615628a506f8
TolyaHuligan <66628747+StupidStrawberry@users.noreply.github.com>
2024-10-28 12:46:21 +0700

    генератор строк
    генерирует строку заданой длинны из русских и англиских букв

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def generate_string(length):
+    alphabet = "йцукенгшщзхъэждлорпавыфячсмитьбюqwertyuioplkjhgfdsazxcvbnm"
+    string = ""
+    for i in range(length):
+        x = random.randint(0, 57)
+        string += alphabet[x]
+    return string
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(generate_string(12))
